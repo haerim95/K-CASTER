932ca4d817d297d26ecc2854b7ac414abb0fd9eb
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2021-10-25 01:38:20 +0900

    file 수정사항

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-  return <div>익명의 캐스터 프로젝트 시작해보자.</div>;
-};
-
-export default Home;
--- pages/mypage/[id].js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Mypage = () => {
-  return <div>마이페이지걸랑</div>;
-};
-
-export default Mypage;
