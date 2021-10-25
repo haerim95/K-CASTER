05b64e36aab45335deea870e5fd534192cb8c433
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2021-10-26 00:43:54 +0900

    eslint setting 및 Link 이동 연결

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import PropTtypes from 'prop-types';
+import Link from 'next/link';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <div>
+        <Link href='/'>
+          <a>익명의 캐스터</a>
+        </Link>
+        <Link href='/profile'>
+          <a>프로필</a>
+        </Link>
+        <Link href='/signup'>
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
