214948f7b4f6ce2683f70f2c80106e5bacc30551
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2021-10-27 01:24:01 +0900

    그리드 적용

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import 'antd/dist/antd.css';
 
 const App = ({ Component }) => {
-  return <Component />;
+  return (
+    <>
+      <Head>
+        <meta charSet='urf-8' />
+        <title>익명의 캐스터</title>
+      </Head>
+      <Component />
+
+      {/* ! 수정해야함... */}
+      <p>
+        <a
+          href='https://github.com/haerim95/anonymous-caster'
+          target='_blank'
+          rel='noreferrer nopener'
+        >
+          Made By Hearim
+        </a>
+      </p>
+    </>
+  );
 };
 
 App.propTypes = {
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 
 const Profile = () => {
   return (
-    <AppLayout>
-      <div>내 프로필ss</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>내 프로필 | 익명의 캐스터</title>
+      </Head>
+      <AppLayout>
+        <div>내 프로필ss</div>
+      </AppLayout>
+    </>
   );
 };
 
