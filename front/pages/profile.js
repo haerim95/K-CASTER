468988b696886d1d619a08ba78dd2b8b3f6444ba
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2021-10-31 03:38:39 +0900

    프로필 페이지 만들기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+  const followerList = [
+    { nickname: '양천구' },
+    { nickname: '서대문구' },
+    { nickname: '강남구' },
+  ];
+  const followingList = [
+    { nickname: '양천구' },
+    { nickname: '서대문구' },
+    { nickname: '강남구' },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필 | 익명의 캐스터</title>
       </Head>
       <AppLayout>
-        <div>내 프로필ss</div>
+        <NicknameEditForm />
+        <FollowList header='팔로잉 목록' data={followingList} />
+        <FollowList header='팔로워 목록' data={followerList} />
       </AppLayout>
     </>
   );
