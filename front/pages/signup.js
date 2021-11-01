bb2e27ae26bb183560a3bca2c1f23a3c631917e5
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2021-11-01 23:56:41 +0900

    form 작성중

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Head from 'next/head';
+import { Form, Input } from 'antd';
 import AppLayout from '../components/AppLayout';
 
 const Signup = () => {
+  const [id, setId] = useState('');
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const [nickname, setNickname] = useState('');
+  const onChangeNickName = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+
+  const [password, setPassword] = useState('');
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(() => {}, []);
+
   return (
-    <>
+    <AppLayout>
       <Head>
         <title>회원가입 | 익명의 캐스터</title>
       </Head>
-      <AppLayout>
-        <div>회원가입 페이지</div>
-      </AppLayout>
-    </>
+      <Form onFinish={onSubmit}>
+        <div>
+          <label htmlFor='user-id'>아이디</label> <br />
+          <Input name='user-id' value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor='user-nick'>닉네임</label> <br />
+          <Input
+            name='user-nick'
+            value={nickname}
+            required
+            onChange={onChangeNickName}
+          />
+        </div>
+        <div>
+          <label htmlFor='user-password'>비밀번호</label> <br />
+          <Input
+            name='user-password'
+            type='password'
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+      </Form>
+    </AppLayout>
   );
 };
 
