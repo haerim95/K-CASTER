a52f81ad421811b04746d78e51506f5525d3eead
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2022-04-22 01:54:24 +0900

    Update: console

--- front/pages/hashtag/[tag].js
+++ front/pages/hashtag/[tag].js
@@ -51,9 +51,9 @@ const Hashtag = () => {
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(async context => {
-  console.log(context);
+  // console.log(context);
   const cookie = context.req ? context.req.headers.cookie : '';
-  console.log(context);
+  // console.log(context);
   axios.defaults.headers.Cookie = '';
   if (context.req && cookie) {
     axios.defaults.headers.Cookie = cookie;
