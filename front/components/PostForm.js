a910ea1663c88be163d7f0cb48b9972158956c08
Hae Rim Kim <63902948+haerim95@users.noreply.github.com>
2022-04-23 03:32:40 +0900

    Fix: image preview

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -175,7 +175,11 @@ const PostForm = () => {
               marginBottom: '5px'
             }}
           >
-            <img src={v} style={{ width: '200px' }} alt={v} />
+            <img
+              src={v.replace(/\/thumb\//, '/original/')}
+              style={{ width: '200px' }}
+              alt={v}
+            />
             <CloseBtn onClick={onRemoveImage(i)}>X</CloseBtn>
           </div>
         ))}
